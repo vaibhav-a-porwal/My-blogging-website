6647333e80fd0cc21e3b1bb17a8ccb2f8f97ab16
Vaibhav A. Porwal <vaibhav83porwal@gmail.com>
2021-03-18 11:12:26 +0530

    rename index.html

--- blogs/views.py
+++ blogs/views.py
@@ -5,7 +5,7 @@ from .models import Blog
 # Create your views here.
 def show_blogs(request):
     blogs = Blog.objects.all()
-    return render(request, 'blogs/blogs.html', {'blogs':blogs})
+    return render(request, 'blogs/index.html', {'blogs':blogs})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog,pk=blog_id)
